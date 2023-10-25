cecb244d487d88280182ab47c20da81b582b6d74
ramsesndame237 <ramsesndame34@gmail.com>
2023-10-25 17:38:18 +0100

    Feat:get files or directories
    write a utils functions to get files or directories
    
    Reviewed-by: Victor
    Refs : #4

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -2,6 +2,7 @@
  * ecrire une fonction qui permet de savoir si c'est un repertoire
  */
 import * as fs from "fs";
+import * as path from "path";
 
 export const isDirectory = (pathString:string) =>{
     return fs.lstatSync(pathString).isDirectory();
@@ -9,6 +10,16 @@ export const isDirectory = (pathString:string) =>{
 
 export const isFile  = (pathString:string) =>{
     return fs.lstatSync(pathString).isFile()
+}
+
+export const getDirectories = (pathString:string) =>{
+    const directories = fs.readdirSync(pathString)
+    return directories.filter((directory) => isDirectory(path.join(pathString,directory))
+}
+
+export const getFiles = (pathString:string) =>{
+    const files = fs.readdirSync(pathString);
+    return files.filter((file) => isFile(path.join(pathString, file)));
 }
 
 
