e15dc7984ae8f730df8a090c56b2b9cae426437f
ramsesndame237 <ramsesndame34@gmail.com>
2023-10-25 17:43:01 +0100

    Feat:create other utils function
    write other utils function to get such as formatTime, extensions, know if is compressed, encrypted and other
    
    Reviewed-by: Victor
    Refs : #5

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -14,7 +14,7 @@ export const isFile  = (pathString:string) =>{
 
 export const getDirectories = (pathString:string) =>{
     const directories = fs.readdirSync(pathString)
-    return directories.filter((directory) => isDirectory(path.join(pathString,directory))
+    return directories.filter((directory) => isDirectory(path.join(pathString,directory)))
 }
 
 export const getFiles = (pathString:string) =>{
@@ -22,4 +22,45 @@ export const getFiles = (pathString:string) =>{
     return files.filter((file) => isFile(path.join(pathString, file)));
 }
 
+export function deleteFile(pathString: string): void {
+    fs.unlinkSync(pathString);
+}
+
+export function deleteDirectory(pathString: string): void {
+    const files = getFiles(pathString);
+    for (const file of files) {
+        deleteFile(path.join(pathString, file));
+    }
+    fs.rmdirSync(pathString);
+}
+
+export function getSize(pathString: string): number {
+    return fs.statSync(pathString).size;
+}
+
+export function getExtension(pathString: string): string {
+    return pathString.split(".").pop();
+}
+
+export function stringifyDate(date: Date): string {
+    return date.toLocaleString();
+}
+
+export function formatTime(time: number): string {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}m ${seconds}s`;
+}
+
+export function isCompressed(pathString: string): boolean {
+    return pathString.endsWith(".zip") || pathString.endsWith(".tar") || pathString.endsWith(".gz");
+}
+
+export function isRemote(pathString: string): boolean {
+    return pathString.startsWith("http://") || pathString.startsWith("https://");
+}
+
+export function isEncrypted(pathString: string): boolean {
+    return pathString.startsWith("password:");
+}
 
