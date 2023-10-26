c771ff8702ca7af77b31dd56260d6b5c512ac0f6
ramsesndame237 <ramsesndame34@gmail.com>
2023-10-26 05:28:06 +0100

    Feat:create the removeFileRecusive
    write the remove files recursive function  in the index.ts file
    
    Reviewed-by: Victor
    Refs : #6

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,118 @@
-import * as fs from 'fs-extra';
-import * as path from 'path';
-import {Options} from "./interfaces/interfaces";
 
-async function removeFilesRecursive(baseDirectory: string, options?: Options): Promise<void> {
-    try {
-        const files: string[] = await fs.readdir(baseDirectory);
+import {Options} from "./interfaces/interfaces";
+import {deleteDirectory, deleteFile, formatTime, getDirectories, getFiles, isDirectory} from "./helpers/utils";
+import * as path from "path";
+
+    const removeFilesRecursive = async (
+    pathString: string,
+    options: Options = {}
+): Promise<void>  => {
+    const {
+        preview = false,
+        verbose = false,
+        extensions = [],
+        sizes = [],
+        onEnd,
+        cancelable = false,
+        output,
+        exportFormat,
+    } = options;
+
+    if (preview) {
+        const files = getFiles(pathString);
+        const directories = getDirectories(pathString);
+
+        if (verbose) {
+            console.log(
+                "Les fichiers et répertoires suivants seront supprimés :"
+            );
+        }
 
         for (const file of files) {
-            const filePath: string = path.join(baseDirectory, file);
-            const fileStats: fs.Stats = await fs.lstat(filePath);
-
-            // Filtrer les fichiers en fonction des options fournies
-            if (options?.allowedExtensions && !options.allowedExtensions.includes(path.extname(file)) ||
-                options?.maxFileSize && fileStats.size > options.maxFileSize) {
-                continue; // Ignorer le fichier et passer au suivant
-            }
-
-            if (fileStats.isFile()) {
-                await fs.unlink(filePath); // Suppression du fichier
-            } else if (fileStats.isDirectory()) {
-                await removeFilesRecursive(filePath, options); // Appel récursif pour les sous-répertoires
+            if (verbose) {
+                console.log(file);
             }
         }
 
-        await fs.rmdir(baseDirectory); // Suppression du répertoire une fois tous les fichiers supprimés
-    } catch (error) {
-        console.error('Erreur lors de la suppression récursive des fichiers :', error);
-    }
-}
+        for (const directory of directories) {
+            if (verbose) {
+                console.log(directory);
+            }
+        }
 
+        return Promise.resolve();
+    }
+
+    const canceled = new Promise<void>((resolve, reject) => {
+        let canceling = false
+        const cancel = () => {
+            canceling = true;
+            resolve();
+        };
+
+        if (cancelable) {
+            process.on("SIGINT", cancel);
+            process.on("SIGTERM", cancel);
+        }
+    });
+
+    let startedAt = Date.now();
+    let filesDeleted = 0;
+    let directoriesDeleted = 0;
+
+    async function removeFilesRecursive(pathString: string) {
+        if (canceled) {
+            return;
+        }
+
+        const isDirectoryPath = isDirectory(pathString);
+
+        if (isDirectoryPath) {
+            const files = getFiles(pathString);
+            const directories = getDirectories(pathString);
+
+            for (const file of files) {
+                await removeFilesRecursive(path.join(pathString, file));
+            }
+
+            for (const directory of directories) {
+                await removeFilesRecursive(path.join(pathString, directory));
+            }
+
+            deleteDirectory(pathString);
+        } else {
+            deleteFile(pathString);
+        }
+
+        filesDeleted++;
+        directoriesDeleted++;
+
+        if (output) {
+            output(pathString);
+        }
+
+        if (verbose) {
+            console.log(`${pathString} supprimé`);
+        }
+    }
+
+    await removeFilesRecursive(pathString);
+
+    const endedAt = Date.now();
+    const timeTaken = endedAt - startedAt;
+
+    if (verbose) {
+        console.log(
+            `${filesDeleted} fichiers et ${directoriesDeleted} répertoires supprimés en ${formatTime(
+                timeTaken
+            )}`
+        );
+    }
+
+    if (onEnd) {
+        onEnd();
+    }
+
+    return canceled;
+}
 export { removeFilesRecursive };
